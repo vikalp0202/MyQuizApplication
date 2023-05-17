ee1ae33c2d3a01d0753696cdc49954b96f7871ba
Sumit <sst7024695996@gmail.com>
2023-05-17 21:29:08 +0530

    buttons

---
+++ app/src/main/java/easy/tuto/myquizapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package easy.tuto.myquizapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    int score=0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+
+        loadNewQuestion();
+
+
+
+
+    }
